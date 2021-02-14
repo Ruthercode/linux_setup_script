c94aa2c02f7865726f7a1b99085af361a710a2a3
Paul <46029027+Ruthercode@users.noreply.github.com>
2021-02-14 16:05:46 +0300

    Add first part of instalation

---
+++ setup_part1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-install python3-pip
+pip3 install tensorflow-gpu numpy keras
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
+mkdir ~/tmp && cd ~/tmp
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update
+
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+
+sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt-get update
+
+sudo apt-get install --no-install-recommends nvidia-driver-450
